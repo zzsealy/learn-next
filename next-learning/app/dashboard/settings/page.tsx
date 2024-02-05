847bfdec15b4741864b0ef6185e61a3b4c083441
dairuiquan <1147575371@qq.com>
2024-02-05 14:54:46 +0800

    feat: layout

---
+++ next-learning/app/dashboard/settings/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1 className="text-4xl text-orange-500">Hello Next.js 这里是dashboard/settings</h1>
+}
---
+++ next-learning/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <h1 className='text-4xl text-orange-600'>Hello HomePage</h1>
+  )
+}
